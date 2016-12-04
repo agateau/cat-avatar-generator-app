d1ed2106013ac735c88bd20376fec4e0cd4bf896
Aurélien Gâteau <mail@agateau.com>
2016-12-04 18:44:26 +0100

    Add crude share UI

--- app/src/main/java/com/agateau/catgenerator/MainActivity.java
+++ app/src/main/java/com/agateau/catgenerator/MainActivity.java
@@ -1,13 +1,26 @@
 package com.agateau.catgenerator;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.agateau.utils.log.NLog;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private final PartDb mPartDb = new PartDb();
@@ -41,21 +54,75 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button button = (Button) findViewById(R.id.shareButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareAvatar();
+            }
+        });
+
         generateAvatar();
     }
 
     private void generateAvatar() {
-        String name = String.valueOf(mNameEditText.getText()).trim().toLowerCase();
-        long seed = 0;
-        for (int idx = 0; idx < name.length(); ++idx) {
-            seed += name.codePointAt(idx);
-        }
-        NLog.i("name=%s seed=%d", name, seed);
+        long seed = computeSeed();
         int size = 1024;
         if (mAvatarGenerator != null) {
             mAvatarGenerator.cancel(true);
         }
         mAvatarGenerator = new AvatarGenerator(this, mPartDb, mImageView, size);
         mAvatarGenerator.execute(seed);
+    }
+
+    private long computeSeed() {
+        String name = String.valueOf(mNameEditText.getText()).trim().toLowerCase();
+        long seed = 0;
+        for (int idx = 0; idx < name.length(); ++idx) {
+            seed += name.codePointAt(idx);
+        }
+        return seed;
+    }
+
+    private void shareAvatar() {
+        long seed = computeSeed();
+        File avatarDir = new File(getCacheDir(), "avatars");
+        avatarDir.mkdirs();
+        File avatarFile = new File(avatarDir, String.valueOf(seed) + ".png");
+
+        if (!saveAvatar(avatarFile)) {
+            return;
+        }
+        NLog.i("Avatar saved to %s", avatarFile);
+
+        Uri contentUri = FileProvider.getUriForFile(this, "com.agateau.catgenerator.fileprovider", avatarFile);
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("image/png");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        startActivity(Intent.createChooser(shareIntent, "Share avatar with..."));
+    }
+
+    private boolean saveAvatar(File avatarFile) {
+        BitmapDrawable drawable = (BitmapDrawable) mImageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+        FileOutputStream stream = null;
+        try {
+            stream = new FileOutputStream(avatarFile);
+        } catch (FileNotFoundException e) {
+            NLog.e("Opening %s failed: %s", avatarFile, e);
+            return false;
+        }
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        try {
+            stream.close();
+        } catch (IOException e) {
+            NLog.e("Failed to close %s: %s", avatarFile, e);
+            return false;
+        }
+        return true;
     }
 }
